b18878759b32af837b947b85a1d4f75a2cf6c041
Laurent Bouquet <bouquet@ndlp.fr>
2024-12-09 11:56:28 +0100

    First commit

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html"], // Indiquez que Tailwind doit analyser le fichier index.html
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
+
